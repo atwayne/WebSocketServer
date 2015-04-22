f46b5daa021024ffabf1195c1e5840c40cc4d1a1
Wayne Wang <lastr2d2@gmail.com>
2015-04-22 22:28:50 +0800

    empty web api project

---
+++ WebSocketServer/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebSocketServer.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Index()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "hello world");
+        }
+    }
+}
---
+++ WebSocketServer/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Http;
+using WebSocketServer.Core;
+
+namespace WebSocketServer
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+        }
+    }
+}
